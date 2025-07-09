a9abe5f3e554675acd9bd3c9fbe35f62850f0881
Rai <raigarciatorres@outlook.com>
2025-07-09 12:18:53 -0300

    segundo commit de outra branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Content } from "./content/content";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Content],
+  imports: [RouterOutlet, Header, Content, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
