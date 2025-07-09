f1a57490390ce31c2aaae7e09e61a4c22bfa1215
Rai <raigarciatorres@outlook.com>
2025-07-09 11:35:44 -0300

    header and content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Content } from "./content/content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Content],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
